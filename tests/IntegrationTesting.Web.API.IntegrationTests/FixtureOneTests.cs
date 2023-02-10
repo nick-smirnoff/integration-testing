2c210913855b1baf445979190cd16ddb26227156
Nick Smirnoff <nick.smirnoff@puregym.com>
2023-02-10 14:17:43 +0000

    created initial test

---
+++ tests/IntegrationTesting.Web.API.IntegrationTests/FixtureOneTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+namespace IntegrationTesting.Web.API.IntegrationTests
+{
+    public class FixtureOneTests : IClassFixture<TestApplicationFactory>, IAsyncLifetime
+    {
+        private readonly TestApplicationFactory _factory;
+        private readonly HttpClient _client;
+
+        public FixtureOneTests(TestApplicationFactory factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        public Task InitializeAsync()
+        {
+            return _factory.InitializeAsync();
+        }
+
+        public Task DisposeAsync()
+        {
+            return _factory.DisposeAsync();
+        }
+
+        [Fact]
+        public async Task GetWeatherForecast_TestOne()
+        {
+            var response = await _client.GetAsync("WeatherForecast/GetWeatherForecast");
+
+            response.IsSuccessStatusCode.Should().BeTrue();
+        }
+    }
+}
